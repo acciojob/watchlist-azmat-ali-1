6f51bbad00a7c7c5610b3e3949fd5cb0455518d6
AZMAT ALI <222azmat@gmail.com>
2022-11-30 13:11:05 +0530

    six

--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -3,11 +3,13 @@ package com.driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Component
 public class MovieController {
 
     @Autowired
--- src/main/java/com/driver/MovieService.java
+++ src/main/java/com/driver/MovieService.java
@@ -1,14 +1,15 @@
 package com.driver;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Component
 public class MovieService {
-
-    @Autowired
+@Autowired
     MovieRepository movieRepository;
 
     //1
